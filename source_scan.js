7de9285926b89e72d7c62319af68e52a553efeb5
Manuel <manuelg99@hotmail.it>
2025-11-14 19:09:03 +0100

    Added comments and refactored

--- source_scan.js
+++ source_scan.js
@@ -1,29 +1,50 @@
-function sources_Scan() {
-    mwContentText = document.getElementById("mw-content-text");
-    if (mwContentText) {
+/**
+ * Scans the page for sources in the main text
+ * @returns the sources structure: category > seasons > episodes
+ */
+function Source_Scan() {
+    // mw-content-text is the id for the main text in fandom.org pages
+    mainText = document.getElementById("mw-content-text");
+    if (mainText) {
         const sources = {};
-        const p_elements = mwContentText.querySelectorAll('p');
+        const p_elements = mainText.querySelectorAll('p');
         p_elements.forEach(p => {
+            // Select the <a> elements that link other wiki pages, these are the links to episodes/movies/series
+            // that are sources to the current paragraph. The <span> element is the small pop up description
+            // that appears when hovering over a source.
             const sourceElements = p.querySelectorAll('a[href^="/wiki/"] > span');
+
+            // Handle each source
             sourceElements.forEach(span => {
+                // Exlude these two tags that use the same a > span elements as the sources
                 if (span.textContent === "edit" || span.textContent === "citation needed")
                     return;
+
+                // The sources with no parenthesis in the pop up <span> are entire series, for example: "Star Trek: The Next Generation"
                 if (!span.title.includes('(')) {
-                    const category = span.title;
-                    if (!sources[category]) {
-                        sources[category] = {
-                            name: category,
+                    const categoryName = span.title;
+                    if (!sources[categoryName]) {
+                        sources[categoryName] = {
+                            name: categoryName,
                             abbreviation: span.textContent,
                             seasons: {}
                         }
                     }
                 }
+                // The sources with parenthesis in the pop up <span> are single episodes or movies
                 else {
-                    const parentContent = span.title.match(/\(([^)]*x[^)]*)\)/);
-                    if (parentContent) // Episode case
-                        add_episode(parentContent[1], sources)
-                    else // Movie case
-                        add_movie(span.title, sources)
+                    // The regex separates the episodes from the movies, by checking what is in the parenthesis
+                    // Important to have the "x" as well, as in the case of episodes from different series but with the same title
+                    // their pop up <span> will have a second parenthesis indicating which series they are from, for example: "Anomaly (DIS) (DIS 4x02)"
+                    const episodeData = span.title.match(/\(([^)]*x[^)]*)\)/);
+                    // Episode case example: "The Battle (TNG 1x09)"
+                    if (episodeData)
+                        add_episode(episodeData[1], sources)
+                    // Movie case example: "Star Trek: First Contact (FLM 08, TNG 2)"
+                    else {
+                        movieData = span.title.match(/\(([^)]+)\)/)[1]; // "FLM 08, TNG 2"
+                        add_movie(movieData, sources)
+                    }
                 }
             });
         })
@@ -32,12 +53,17 @@ function sources_Scan() {
     return [];
 }
 
-function add_episode(parentContent, sources) {
-    const parts = parentContent.split(' ');
-    const abbreviation = parts[0];
-    const episode_data = parts[1].split('x');
-    const season = episode_data[0];
-    const episode = episode_data[1];
+/**
+ * Adds an episode to the sources structure
+ * @param episodeData: string structured like "TNG 1x09"
+ * @param sources: the sources structure 
+ */
+function add_episode(episodeData, sources) {
+    let episodeData_split = episodeData.split(' '); // "TNG" and "1x09"
+    let abbreviation = episodeData_split[0]; // "TNG"
+    let season_and_episode = episodeData_split[1].split('x'); // "1" and "09"
+    const season = season_and_episode[0]; // "1"
+    const episode = season_and_episode[1]; // "09"
     Object.values(sources).forEach(category => {
         if (category.abbreviation === abbreviation) {
             if (!category.seasons[season]) 
@@ -45,14 +71,20 @@ function add_episode(parentContent, sources) {
             if (!category.seasons[season].includes(episode))
                 category.seasons[season].push(episode);
         }
+        // Because all sources are structured like "TNG: "The Battle"", there is no need to check if the
+        // category is not present
     });
 }
 
-function add_movie(title, sources) {
-    parentContent = title.match(/\(([^)]+)\)/)[1];
-    const movie_data = parentContent.split(', ');
-    const movie_number = movie_data[0].split(' ')[1];
-    const movie_era = movie_data[1].split(' ')[0];
+/**
+ * Adds a movie to the sources structure
+ * @param movieData: string structured like "FLM 08, TNG 2"
+ * @param sources: the sources structure 
+ */
+function add_movie(movieData, sources) {
+    const movieData_split = movieData.split(', '); // "FLM 08" and "TNG 2"
+    const movie_number = movieData_split[0].split(' ')[1]; // "08"
+    const movie_era = movieData_split[1].split(' ')[0]; // "TNG"
     Object.values(sources).forEach(category =>  {
         if (category.abbreviation === "FLM") {
             if (!category.seasons[movie_era])
--- spoiler_control.js
+++ spoiler_control.js
@@ -1,60 +1,74 @@
-function setUpPanel(spoilerUI_Button) {
-    spoilerUI_Button.addEventListener('click', () => {
-        let panel = document.getElementById('spoiler-control-panel');
-        if(!panel) {
-            // Add the panel itself
-            panel = document.createElement('div');
-            panel.className = "spoiler-control-panel";
-            panel.id = 'spoiler-control-panel';
+/**
+ * Adds a button element
+ * @param textContent: the textContent of the button element
+ * @param className: the class of the button element 
+ * @returns: the spoiler button element
+ */
+function addButton(textContent, className) {
+    var button = document.createElement('Button');
+    button.textContent = textContent;
+    button.className = className;
+    return button;
+}
 
-            // Add a close button for the panel
-            const closeButton = document.createElement('button');
-            closeButton.textContent = 'Close';
-            closeButton.className = "close-button";
-            closeButton.addEventListener('click', () => {
-                panel.style.display = 'none';
-            });
-            panel.appendChild(closeButton);
+/**
+ * Sets up the spoiler panel on the right side of the screen, filling it with the sources in the page.
+ */
+function setUpPanel() {
+    let panel = document.getElementById('spoiler-control-panel');
+    if (!panel) {
+        panel = document.createElement('div');
+        panel.className = "spoiler-control-panel";
+        panel.id = 'spoiler-control-panel';
 
-            // Fill the panel with the categories, seasons and episodes
-            Object.values(sources).forEach(category => {
-                const categoryDetails = addDetails_and_checkbox(category.name)
-                const seasonList = document.createElement('ul');
-                seasonList.className = "season-list";
-                Object.keys(category.seasons).forEach(season => {
-                    const seasonElement = document.createElement('li');
-                    seasonList.appendChild(seasonElement);
-                    const seasonDetails = addDetails_and_checkbox(`Season ${season}`);
-                    seasonElement.appendChild(seasonDetails);
-                    category.seasons[season].forEach(episode => {
-                        const episodeList = document.createElement('ul');
-                        episodeList.className = "episode-list";
-                        seasonDetails.appendChild(episodeList);
-                        const episodeElement = document.createElement('li');
-                        episodeElement.appendChild(document.createTextNode(episode));
-                        add_checkbox(episodeElement);
-                        episodeList.appendChild(episodeElement);
-                    });
+        const closeButton = addButton('Close', 'close-button');
+        closeButton.addEventListener('click', () => {
+            panel.style.display = 'none';
+        });
+        panel.appendChild(closeButton);
+
+        // Fill the panel with the categories, seasons and episodes
+        // Follows this hierarchy: details > ul > li > details > ul > li
+        // Categories
+        Object.values(sources).forEach(category => {
+            const categoryDetails = addDetails_and_checkbox(category.name)
+
+            // Seasons for each category
+            const seasonList = document.createElement('ul');
+            seasonList.className = "season-list";
+            Object.keys(category.seasons).forEach(season => {
+                const seasonElement = document.createElement('li');
+                seasonList.appendChild(seasonElement);
+                const seasonDetails = addDetails_and_checkbox(`Season ${season}`);
+                seasonElement.appendChild(seasonDetails);
+
+                // Episodes for each season
+                category.seasons[season].forEach(episode => {
+                    const episodeList = document.createElement('ul');
+                    episodeList.className = "episode-list";
+                    seasonDetails.appendChild(episodeList);
+                    const episodeElement = document.createElement('li');
+                    episodeElement.appendChild(document.createTextNode(episode));
+                    add_checkbox(episodeElement);
+                    episodeList.appendChild(episodeElement);
                 });
-                categoryDetails.appendChild(seasonList);
-                panel.appendChild(categoryDetails);
-            })
+            });
+            categoryDetails.appendChild(seasonList);
+            panel.appendChild(categoryDetails);
+        })
 
-            document.body.appendChild(panel);
-        } else {
-            panel.style.display = panel.style.display === 'none' ? 'block' : 'none';
-        }
-    });
+        document.body.appendChild(panel);
+    } else {
+        // Panel is already set up
+        panel.style.display = panel.style.display === 'none' ? 'block' : 'none';
+    }
 }
 
-function addSpoilerButton() {
-    const spoiler_button = document.createElement('Button');
-    spoiler_button.textContent = 'Spoilers control';
-    spoiler_button.className = "spoiler-button";
-    document.body.appendChild(spoiler_button);
-    setUpPanel(spoiler_button);
-}
-
+/**
+ * Adds a details element with a checkbox in its summary element
+ * @param summaryText: the text displayed on screen 
+ * @returns the added details element
+ */
 function addDetails_and_checkbox(summaryText) {
     const details = document.createElement('details');
     const summary = document.createElement('summary');    
@@ -64,16 +78,25 @@ function addDetails_and_checkbox(summaryText) {
     return details;
 }
 
+/**
+ * Adds a checkbox input element
+ * @param fatherElement: the element that the checkbox is going to be appended to
+ */
 function add_checkbox(fatherElement) {
     const checkbox = document.createElement('input');
     checkbox.type = "checkbox";
     fatherElement.insertBefore(checkbox, fatherElement.firstChild);
 }
 
-const sources = sources_Scan();
+// Main
+const sources = Source_Scan();
 const head = document.getElementsByTagName('head')[0];
 const link = document.createElement('link');
 link.rel = 'stylesheet';
 link.href  = 'panelStye.css';
 link.type = 'text/css';
-addSpoilerButton();
+const spoilerButton = addButton('Spoilers Control', 'spoiler-button');
+document.body.appendChild(spoilerButton);
+spoilerButton.addEventListener('click', () => {
+    setUpPanel();
+})
