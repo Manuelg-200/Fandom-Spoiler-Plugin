dde1d15029eca0344794d1948470e839a2a3e194
Manuel <manuelg99@hotmail.it>
2025-11-15 18:03:52 +0100

    Added titles in the struct to display

--- source_scan.js
+++ source_scan.js
@@ -22,7 +22,7 @@ function Source_Scan() {
 
                 // The sources with no parenthesis in the pop up <span> are entire series, for example: "Star Trek: The Next Generation"
                 if (!span.title.includes('(')) {
-                    const categoryName = span.title;
+                    let categoryName = span.title;
                     if (!sources[categoryName]) {
                         sources[categoryName] = {
                             abbreviation: span.textContent,
@@ -35,10 +35,13 @@ function Source_Scan() {
                     // The regex separates the episodes from the movies, by checking what is in the parenthesis
                     // Important to have the "x" as well, as in the case of episodes from different series but with the same title
                     // their pop up <span> will have a second parenthesis indicating which series they are from, for example: "Anomaly (DIS) (DIS 4x02)"
-                    const episodeData = span.title.match(/\(([^)]*x[^)]*)\)/);
+                    let episodeData = span.title.match(/\(([^)]*x[^)]*)\)/);
+                    // Removes what's between parenthesis to isolate the title
+                    let title = span.title.replace(/\s*\([^)]*\)\s*$/, "");
+
                     // Episode case example: "The Battle (TNG 1x09)"
                     if (episodeData)
-                        add_episode(episodeData[1], sources)
+                        add_episode(episodeData[1], title, sources)
                     // Movie case example: "Star Trek: First Contact (FLM 08, TNG 2)"
                     else {
                         // Add the category for movies here, as it's a special case
@@ -49,8 +52,9 @@ function Source_Scan() {
                                 seasons: {} // Actually used for the Era of the movie
                             }
                         }
-                        movieData = span.title.match(/\(([^)]+)\)/)[1]; // "FLM 08, TNG 2"
-                        add_movie(movieData, sources)
+                        let movieData = span.title.match(/\(([^)]+)\)/)[1]; // "FLM 08, TNG 2"
+                        let title = span.title.replace(/\s*\([^)]*\)\s*$/, "");
+                        add_movie(movieData, title, sources)
                     }
                 }
             });
@@ -63,20 +67,22 @@ function Source_Scan() {
 /**
  * Adds an episode to the sources structure
  * @param episodeData: string structured like "TNG 1x09"
+ * @param title: episode title string
  * @param sources: the sources structure 
  */
-function add_episode(episodeData, sources) {
+function add_episode(episodeData, title, sources) {
     let episodeData_split = episodeData.split(' '); // "TNG" and "1x09"
     let abbreviation = episodeData_split[0]; // "TNG"
     let season_and_episode = episodeData_split[1].split('x'); // "1" and "09"
-    const season = season_and_episode[0]; // "1"
-    const episode = season_and_episode[1]; // "09"
+    let season = season_and_episode[0]; // "1"
+    let episode = season_and_episode[1]; // "09"
+    let stringToAdd = `${episode}: ${title}`;
     Object.values(sources).forEach(category => {
         if (category.abbreviation === abbreviation) {
             if (!category.seasons[season]) 
                 category.seasons[season] = [];
-            if (!category.seasons[season].includes(episode))
-                category.seasons[season].push(episode);
+            if (!category.seasons[season].includes(stringToAdd))
+                category.seasons[season].push(stringToAdd);
         }
         // Because all sources are structured like "TNG: "The Battle"", there is no need to check if the
         // category is not present
@@ -86,18 +92,18 @@ function add_episode(episodeData, sources) {
 /**
  * Adds a movie to the sources structure
  * @param movieData: string structured like "FLM 08, TNG 2"
+ * @param title: movie title string
  * @param sources: the sources structure 
  */
-function add_movie(movieData, sources) {
-    const movieData_split = movieData.split(', '); // "FLM 08" and "TNG 2"
-    const movie_number = movieData_split[0].split(' ')[1]; // "08"
-    const movie_era = movieData_split[1].split(' ')[0]; // "TNG"
+function add_movie(movieData, title, sources) {
+    let movieData_split = movieData.split(', '); // "FLM 08" and "TNG 2"
+    let movie_era = movieData_split[1].split(' ')[0]; // "TNG"
     Object.values(sources).forEach(category =>  {
         if (category.abbreviation === "FLM") {
             if (!category.seasons[movie_era])
                 category.seasons[movie_era] = [];
-            if (!category.seasons[movie_era].includes(movie_number))
-                category.seasons[movie_era].push(movie_number);
+            if (!category.seasons[movie_era].includes(title))
+                category.seasons[movie_era].push(title);
         }
     })
 }
